2e20ce6d05a555b29c070bbb478d95b41ef49a26
Minezss <69091939+Minezss@users.noreply.github.com>
2022-10-21 16:56:19 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,21 +1,47 @@
-var weight = [104, 95, 76, 92]
+var sea,ship;
+var seaImg,shipImg;
 
-function average_weight()
-{
-  var sum = weight[0]+weight[1]+weight[2]+weight[3]
-  var avg = sum/weight.length
-  console.log(avg)
+function preload(){
+  //uncomment the code to add animation to ship 
+
+  //shipImg1 = loadAnimation("ship-1.png");
+  //shipImg1 = loadAnimation("ship-1.png");
+  //shipImg1 = loadAnimation("ship-1");
+  shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
+  //shipImg1 = loadAnimation("ship-1","ship-2","ship-1","ship-2");
+  
+  seaImg = loadImage("sea.png");
 }
 
-function setup() 
-{
+function setup(){
   createCanvas(400,400);
-  average_weight();
+  background("blue");
+
+  // Moving background
+  sea=createSprite(400,200);
+  sea.addImage(seaImg);
+  sea.velocityX = -5;
+  sea.scale=0.3;
+
+  
+  ship = createSprite(130,200,30,30);
+  ship.addAnimation("movingShip",shipImg1);
+  ship.scale =0.25;
+  
 }
 
-function draw() 
-{
-background(51);
+function draw() {
+  background(0);
+  sea.velocityX = -3;
 
+  //uncomment code to reset the background
+  if(sea.x < 0){
+    //sea.x = 0;
+    //sea.x = sea.width;
+    sea.x = sea.width/8;
+    //sea.y = height;
+  }
+
+ 
+  drawSprites();
 }
-
